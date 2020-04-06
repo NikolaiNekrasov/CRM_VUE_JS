9540aef7b9e77e6d176e397506bdcbc6e8bf5293
NikolaiNekrasov <nikolai.nekrasov@gmail.com>
2020-04-06 18:33:56 +0300

    Сделано чтобы по клику убирался sidebar

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="app-main-layout">
-    <Navbar />
-    <Sidebar />
-    <main class="app-content">
+    <Navbar @click="isOpen = !isOpen" />
+    <Sidebar v-model="isOpen" />
+    <main class="app-content" :class="{full: !isOpen}">
       <div class="app-page">
         <router-view />
       </div>
@@ -21,6 +21,9 @@ import Navbar from '@/components/app/Navbar'
 import Sidebar from '@/components/app/Sidebar'
 export default {
   name: 'main-layout',
+  data: () => ({
+    isOpen: true
+  }),
   components: {
     Navbar, Sidebar
   }
