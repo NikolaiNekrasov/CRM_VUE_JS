cef09f1321eade4500afcca03bfbbbfc56dbdd4e
NikolaiNekrasov <nikolai.nekrasov@gmail.com>
2020-04-06 17:19:20 +0300

    Добавил все необходимые views  и обновил router

--- src/router/index.js
+++ src/router/index.js
@@ -17,10 +17,46 @@ const routes = [
     component: () => import('../views/Login.vue')
   },
   {
+    path: '/register',
+    name: 'register',
+    meta: { layout: 'empty' },
+    component: () => import('../views/Register.vue')
+  },
+  {
     path: '/categories',
     name: 'categories',
     meta: { layout: 'main' },
     component: () => import('../views/Categories.vue')
+  },
+  {
+    path: '/detail',
+    name: 'detail',
+    meta: { layout: 'main' },
+    component: () => import('../views/Detail.vue')
+  },
+  {
+    path: '/history',
+    name: 'history',
+    meta: { layout: 'main' },
+    component: () => import('../views/History.vue')
+  },
+  {
+    path: '/planning',
+    name: 'planning',
+    meta: { layout: 'main' },
+    component: () => import('../views/Planning.vue')
+  },
+  {
+    path: '/profile',
+    name: 'profile',
+    meta: { layout: 'main' },
+    component: () => import('../views/Profile.vue')
+  },
+  {
+    path: '/record',
+    name: 'record',
+    meta: { layout: 'main' },
+    component: () => import('../views/Record.vue')
   }
 ]
 
